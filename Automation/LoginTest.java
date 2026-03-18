cc33bc554c85b01f82a7fd8fffae3d5df7f0af08
pavithrabyreddy <pavithra.byreddy987@gmail.com>
2026-03-18 15:01:21 +0530

    Added automation test scripts

---
+++ Automation/LoginTest.java
@@ -0,0 +1,25 @@
+package ecommerce;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+
+        driver.quit();
+    }
+}
